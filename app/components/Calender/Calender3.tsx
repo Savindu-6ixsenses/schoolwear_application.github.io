468c2aa85ba79ed0a6361d35aeee8cce0731edf1
SavixDR <savindu.20@cse.mrt.ac.lk>
2025-02-05 22:39:20 +0530

    Code Refactoring-ESLint Rules -v2

--- app/components/Calender/Calender3.tsx
+++ app/components/Calender/Calender3.tsx
@@ -1,26 +1,34 @@
 import { useState } from "react";
-import Datepicker from "react-tailwindcss-datepicker";
+import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
 
 const NEXT_MONTH = new Date();
-NEXT_MONTH.setMonth(NEXT_MONTH.getMonth()+1);
+NEXT_MONTH.setMonth(NEXT_MONTH.getMonth() + 1);
+
+interface DateRange {
+	startDate: Date;
+	endDate: Date;
+}
 
 interface CalenderProps {
-	onDateChange: (dateRange: { startDate: Date; endDate: Date }) => void;
+	onDateChange: (dateRange: DateRange) => void;
 }
 
 const Calender: React.FC<CalenderProps> = ({ onDateChange }) => {
-	const [value, setValue] = useState<any>({
+	const [value, setValue] = useState<DateRange>({
 		startDate: new Date(),
 		endDate: NEXT_MONTH,
 	});
 
-	const handleChange = (newValue: any) => {
-		setValue(newValue);
+	const handleChange = (newValue: DateValueType | null) => {
+		if (!newValue) return;
 
-		onDateChange({
-			startDate: newValue.startDate,
-			endDate: newValue.endDate,
-		});
+		const formattedDateRange: DateRange = {
+			startDate: new Date(newValue.startDate|| ""),
+			endDate: new Date(newValue.endDate || ""),
+		};
+
+		setValue(formattedDateRange);
+		onDateChange(formattedDateRange);
 	};
 
 	return (
